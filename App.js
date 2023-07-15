aaec9fd97de0378c62ff2dde2ffa69bdcd7395e2
vipul513 <vipulmehta513@gmail.com>
2023-07-15 17:04:03 +0530

    Episode-3

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"}, [
-        React.createElement("h1", {}, "This is namaste react 🚀"),
-        React.createElement("h2", {}, "Hello From React2")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "Hello From React"),
-        React.createElement("h2", {}, "Hello From React2")
-    ]),
-]);
-const heading = React.createElement("h1", {}, "Hello From React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// React Element JSX
+const jsxHeading = (
+    <h1 id="heading" tabIndex={5}> 
+        Namaste Javascript Episode-3 🚀🚀
+    </h1>
+);
+
+// components
+// 1) Class based components  -- OLD
+// 2) Functional based components -- NEW
+
+// react element 
+const heading = <h1>Namaste React Heading</h1>
+
+const Title = () => (
+    <div id="container">
+        <h2> Learning React </h2>
+    </div>
+)
+// react functional component
+const HeadingComponent = () => (
+    <div id="check">
+        <Title />
+        <h1 className="heading">Namaste React 🚀🚀 Heading Component </h1>
+    </div>
+);
+
+
+const rootElm = ReactDOM.createRoot(document.getElementById("root"));
+
+// if you want to render react component write it in <../>
+rootElm.render(<HeadingComponent/>);
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Namaste-React 🚀
 Learning React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR --> Hot Module Replacement
+- File Watching Algorithm --> That will help us to automatic render updated components
+- Faster build --> Bcz of Caching
+- Image Optimisation
+- Minification of file
+- Bundling of files
+- Compress files
+- Consistent hashing
+- Code Spliting
+- Differential bundling
+- Diagonstic
+- Error Handling
+- HTTPS
+
+
+# Babel
+Babel is converted jsx code into HTML element.
+JSX --> Babel transiled i to --> React.createElement --> React element [object] --> render (HTML element) 
