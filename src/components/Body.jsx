67e726b35b3475bd65834acfdf820d84d020d6f4
vipul513 <vipulmehta513@gmail.com>
2023-07-20 01:52:15 +0530

    Episode-5

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,28 @@
 import { resList } from "../fixtures/mockData";
 import { RestaurantCard } from "./RestaurantCard";
+import { useState } from "react";
+
 
 export const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button id="filter-btn"
+          className="btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
